a9a37c891901b88e56aa99d432880c15a79c9b8d
Sara T <sahara.tijol@gmail.com>
2020-11-02 16:31:06 -0600

    Added a printf statement to the hello world servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -12,5 +12,6 @@ public class HelloWorldServlet extends HttpServlet {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello, World!</h1>");
+        out.printf("<h2>Hello, %s", "Sara");
     }
 }
