b7bca4cbb6e59adfca381eb7df7baf5f8ea0f70d
Sara T <sahara.tijol@gmail.com>
2020-11-04 11:40:52 -0600

    Lecture notes: HelloServlet and hello.jsp are added

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World!";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        } else if (name.equals("elonm")) {
+            response.sendRedirect("https://tesla.com");
+            return;
+        } else if (name.equals("jbezos")) {
+            response.sendRedirect("https://amazon.com");
+            return;
+        }
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+    }
+}
