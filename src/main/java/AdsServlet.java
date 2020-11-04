da3e7cc50e729712342f616a6affd7d82c38a3fa
Sara T <sahara.tijol@gmail.com>
2020-11-04 13:41:29 -0600

    AdsServlet returns correct response

---
+++ src/main/java/AdsServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AdsServlet", urlPatterns = "/ads")
+public class AdsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        request.getRequestDispatcher("ads/index.jsp").forward(request, response);
+    }
+}
